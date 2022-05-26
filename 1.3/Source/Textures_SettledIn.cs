4afd0cb3b41e7d64ad76e9934c59cdc933020332
Daniel Renner <daniel.renner@gmx.com>
2022-05-26 17:52:52 +0200

    project has been set up

---
+++ 1.3/Source/Textures_SettledIn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace DanielRenner.SettledIn
+{
+    [StaticConstructorOnStartup]
+    public static class Textures_SettledIn
+    {
+        //public static Texture2D RennFiber = ContentFinder<Texture2D>.Get("RennFiber/RennFiber", false);
+    }
+}
